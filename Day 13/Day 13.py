98365eafb56e8ee768462923996b1764fe9a3270
Gert Jan Bakker <bakkrjangert@hotmail.com>
2021-12-13 20:00:38 +0100

    Update day 13 --> Make set before printing for speed increase

--- Day 13/Day 13.py
+++ Day 13/Day 13.py
@@ -1,7 +1,7 @@
 from termcolor import colored
 import turtle
 
-with open('extreme2.txt') as f:
+with open('tweakers.txt') as f:
     lines = f.read().splitlines()
 
 print_turtle = True
@@ -38,10 +38,12 @@ for instruction in instructions:
         print(f'Part 1: There are {amount} points visible after one fold')
         first_fold = False
 
+tup_coords = map(tuple, coords)
+coords = set(tup_coords)
 print('\nPart 2:\n')
 for y in range(boundaries['y']):
     for x in range(boundaries['x']):
-        if [x, y] in coords:
+        if (x, y) in coords:
             print(colored('@', color='yellow'), end='')
         else:
             print(' ', end='')
@@ -49,18 +51,18 @@ for y in range(boundaries['y']):
 
 # Turtle part 2
 if print_turtle:
-    scale = 4
+    scale = 3
     turtle.delay(0), turtle.hideturtle(), turtle.speed(7.5), turtle.tracer(0, 0), turtle.color('black'), turtle.width(0.7 * scale)
     delta_x, delta_y = -max(coords, key=lambda x: x[0])[0] // 2,  -max(coords, key=lambda x: x[1])[1] // 2
 
     for x in range(boundaries['x']):
         for y in range(boundaries['y']):
-            if [x, y] in coords:
+            if (x, y) in coords:
                 for dx in (-1, 0, 1):
                     for dy in (-1, 0, 1):
                         if dx == dy == 0:       # Skip current point
                             continue
-                        if [x + dx, y + dy] in coords:
+                        if (x + dx, y + dy) in coords:
                             turtle.up(), turtle.goto((x + delta_x) * scale, -(y + delta_y) * scale)
                             turtle.down(), turtle.goto(((x + delta_x) + dx) * scale, -((y + delta_y) + dy) * scale)
     turtle.Screen().update(), turtle.done()
